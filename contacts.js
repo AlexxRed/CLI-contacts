c4a1912a276fb800133266f8cab79a20dcc104a7
Alexandr Krasnobokyi <krasnoboky@gmail.com>
2022-06-13 17:26:45 +0300

    basic app

--- contacts.js
+++ contacts.js
@@ -1,2 +1,51 @@
 const fs = require('fs/promises');
 // const fs = require('fs').promises;
+const path = require('path');
+const contactsPath = 1
+
+// const fileOperations = async (filePath, action = "read", data) => {
+//     switch (action) {
+//         case "read":
+//             const text = await fs.readFile(filePath, "utf-8");
+//             // const text = text.toString()
+//             console.log(text);
+//             break;
+//         case "add":
+//             await fs.appendFile(filePath, data);
+//             break;
+//         case "replace":
+//             await fs.writeFile(filePath, data);
+//             break;
+//         default:
+//             break;
+//     }
+// };
+
+// fileOperations('./db/contacts.json', 'read');
+
+
+// TODO: задокументировать каждую функцию
+async function listContacts() {
+    const result = await fs.readFile('./db/contacts.json', 'utf-8');
+    const contacts = JSON.parse(result);
+    return contacts;
+}
+
+function getContactById(contactId) {
+  // ...твой код
+}
+
+function removeContact(contactId) {
+  // ...твой код
+}
+
+function addContact(name, email, phone) {
+  // ...твой код
+}
+
+module.exports = {
+    listContacts,
+    getContactById,
+    removeContact,
+    addContact
+};
--- index.js
+++ index.js
@@ -1,1 +1,31 @@
-console.log("start backend server");
+const contactsOperations = require('./contacts');
+// const argv = require('yargs').argv;
+
+// TODO: рефакторить
+async function invokeAction({ action, id, name, email, phone }) {
+    switch (action) {
+        case 'list':
+            const result = await contactsOperations.listContacts()
+            console.table(result);
+        break;
+
+        case 'get':
+        // ... id
+        break;
+
+        case 'add':
+        // ... name email phone
+        break;
+
+        case 'remove':
+        // ... id
+        break;
+
+        default:
+        console.warn('\x1B[31m Unknown action type!');
+    }
+}
+
+// invokeAction(argv);
+
+invokeAction({action:'list'})
